c151e7b3a6b90eff348addda343fc42d10a6cbe6
tinyynoob <starcraft2empire@gmail.com>
2021-05-16 20:03:29 +0800

    version 1.3

--- LDPC_SPA/analysis.m
+++
@@ -1,9 +0,0 @@
-clear all;
-M = int8(readmatrix('analysis.csv'));
-u = size(M);
-totalBitNum = u(1)*(u(2)-1);    %subtract the extra column
-
-M = M';
-s = sum(M);
-bit_error_rate = sum(s) / totalBitNum;
-frame_error_rate = sum(logical(s)) / length(s);

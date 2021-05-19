ee798fa8c0e690b05b7c1be66269aff70890207f
tinyynoob <starcraft2empire@gmail.com>
2021-05-19 21:58:57 +0800

    copy from min_sum

--- LDPC_min_sum/main.c
+++ LDPC_min_sum/main.c
@@ -6,9 +6,9 @@ int main()
 	FILE* f;
 	int i;
 	double* er;
-	int loopNum[] = {2000,3000,8000,50000,200000,500000,500000,500000};
-	int max_iteration[] = {16,16,16,16,16,16,16,16};
-	double SNR_db[] = { 1.4,1.6,1.8,2,2.2,2.4,2.4,2.4 };
+	int loopNum[] = {200000,200000,200000};
+	int max_iteration[] = {16,16,16};
+	double SNR_db[] = { 2.4,2.4,2.4};
 	rand_init();
 	er = malloc(sizeof(double) * 2);	//error_rate	er[0]=BER, er[1]=FER
 	f = fopen("./analysis.csv", "w");

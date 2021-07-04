2e2521c5dc2c3b65f13e4e9dc7cc8866843c813a
tinyynoob <starcraft2empire@gmail.com>
2021-07-04 15:42:26 +0800

    scaled min-sum check version completed

---
+++ LDPC_min_sum_scaled/Header.h
@@ -0,0 +1,15 @@
+#pragma once
+#pragma warning(disable:4996)
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+//void min_sum(int, int, double, double*);
+short sgn(double x);
+double input(short, double);
+void rUpdate(int, int, double*, double**, double**, int**);
+int end_condition_check(int, int*, short*, int**);
+int bit_error_count(int, short*);
+void freee(int, int*, int*, int**, double*, double**, double**, short*);
+
+
